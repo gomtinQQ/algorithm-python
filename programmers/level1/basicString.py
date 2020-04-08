5eac2b2f91da7d0d0da0d6dc9bb774c9987ee3ea
Minhyeon Lee <minhyeonlee1@gmail.com>
2020-04-08 22:13:17 +0900

    programmers LEVEL1: basicString, countChar, intSort, starRectangle, stringSort

---
+++ programmers/level1/basicString.py
@@ -0,0 +1,22 @@
+'''
+문제: 문자열 다루기 기본
+
+문제 설명:
+문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
+
+제한 사항:
+1) s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+'''
+def solution(s):
+    if(len(s)==4 or len(s)==6):
+        try:
+            s = int(s)
+            return True
+        except ValueError:
+            return False
+    else:
+        return False
+
+s = input()
+print(solution(s))
